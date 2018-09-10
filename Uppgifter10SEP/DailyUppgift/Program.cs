780841a0f9f86d920c501ddab24f8686b4903100
Drohano <joix99@live.se>
2018-09-10 12:24:47 +0200

    initial commit + Switch case and do loop

---
+++ Uppgifter10SEP/DailyUppgift/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uppgifter10SEP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int start = 1;
+
+            do
+
+            {
+                int n;
+
+                Console.WriteLine("Pick a programm \n 1 , 2 , 3 , 4 , 5 , 6 , 7, 8 \n Press 0 then enter to exit");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                switch (n)
+                {
+                    case 1:
+
+
+                        start = 1;
+                        break;
+
+
+                    case 2:
+
+
+                        start = 1;
+                        break;
+
+
+                    case 3:
+
+
+                        start = 1;
+                        break;
+
+
+
+                    case 4:
+
+
+                        start = 1;
+                        break;
+
+
+
+                    case 5:
+
+
+                        start = 1;
+                        break;
+
+
+                    case 6:
+
+
+                        start = 1;
+                        break;
+
+
+                    case 7:
+
+
+                        start = 1;
+                        break;
+
+
+                    case 8:
+
+
+                        start = 1;
+                        break;
+
+
+                    case 0:
+                        start = 0;
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Whoops something went wrong try again. \n");
+                            start = 1;
+                        break;
+                }
+  
+            }
+
+            while (start != 0);
+        }
+    }
+}
